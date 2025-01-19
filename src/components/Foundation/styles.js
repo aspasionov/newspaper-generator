7a1b50de01324ccc5f84f3f1b35c8920de30f615
Artem Spasionov <spasia3run@gmail.com>
2025-01-19 15:56:52 +0200

    feat: add two blocks

---
+++ src/components/Foundation/styles.js
@@ -0,0 +1,15 @@
+const IS1 = { borderSpacing: 0, borderBottom: '2px solid #233043', margin: '20px 0 0', maxWidth: '672px' }
+
+const IS2 = { padding: '0 15px 0 0' }
+
+const IS3 = { borderBottom: '50px solid #233043', borderLeft: '0px solid transparent', borderRight: '40px solid transparent', height: 0, padding: '0 20px', color: 'white', width: 'fit-content', textTransform: 'uppercase', textAlign: 'center', lineHeight: '50px', fontSize: '20px', fontWeight: 'bold', fontStretch: 'normal' }
+
+const IS4 = { padding: '0 20px 20px' }
+
+const IS5 = { width: '632px', marginTop: '20px' }
+
+const IS6 = { width: '672px' }
+
+const IS7 = { fontSize: '14px', fontWeight: 'normal', fontStretch: 'normal', fontStyle: 'normal', lineHeight: '1.57', letterSpacing: 'normal', textAlign: 'left', color: '#54555e', paddingLeft: '10px', wordWrap: 'break-word' }
+
+export { IS1, IS2, IS3, IS4, IS5, IS6, IS7 }
